8f4f601b5f66a17e7823a487404c20a653ee811c
travisjlo <travislohome@gmail.com>
2026-01-16 11:06:54 -0700

    added more cal events

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
  // Very basic service worker - cache important files
 const CACHE_NAME = 'my-pwa-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/pair.html',
-  '/events.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/dance-app/',
+  '/dance-app/index.html',
+  '/dance-app/pair.html',
+  '/dance-app/events.html',
+  '/dance-app/style.css',
+  '/dance-app/app.js',
+  '/dance-app/manifest.json',
+  '/dance-app/icons/icon-192.png',
+  '/dance-app/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
